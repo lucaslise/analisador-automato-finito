52a1a9873ae7a4c1f8f226bdaa2e22b389f9b80e
Lucas Lise <lucas_lise1@hotmail.com>
2018-11-06 14:40:50 -0200

    Add mask

--- src/components/GramaticaRegular.js
+++ src/components/GramaticaRegular.js
@@ -1,4 +1,4 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { AutoComplete, Button } from 'antd';
 import _ from 'lodash';
@@ -12,12 +12,17 @@ class GramaticaRegular extends Component {
     }],
   };
 
-  handleChange = (rule) => (value) => {
+  handleChange = rule => (value) => {
     let { rules } = this.state;
 
     rules = _.map(rules, (r) => {
       if (r.name === rule.name) {
-        r.value = value.slice(6);
+        let v = value.slice(6);
+        const valueSize = v.length;
+
+        if (rule.value.length < valueSize && v[valueSize - 1] === ' ' && v[valueSize - 2] !== '|') v += '| ';
+
+        r.value = v;
       }
 
       return r;
@@ -47,7 +52,7 @@ class GramaticaRegular extends Component {
         name: nextRuleName,
         value: '',
       },
-    ]
+    ];
 
     this.props.onChangeRules(newRules);
 
@@ -57,10 +62,10 @@ class GramaticaRegular extends Component {
     });
   }
 
-  handleDeleteRule = (rule) => () => {
+  handleDeleteRule = rule => () => {
     const { rules } = this.state;
 
-    const newRules = _.reject(rules, (r) => r === rule);
+    const newRules = _.reject(rules, r => r === rule);
 
     this.props.onChangeRules(newRules);
 
@@ -75,24 +80,22 @@ class GramaticaRegular extends Component {
   getFields = () => {
     const { rules } = this.state;
     return _.map(rules, (rule, index) => (
-        <div>
-          <AutoComplete
-            key={rule.name}
-            onSearch={this.handleSearch}
-            onChange={this.handleChange(rule)}
-            value={`${rule.name} ::= ${rule.value}`}
-           />
-         <Button size="small" title="Remover Regra" onClick={this.handleDeleteRule(rule)} icon="delete" type="danger" style={{ marginLeft: 10, display: rule.initial ? 'none' : '' }}></Button>
-          <Button size="small" onClick={this.handleNewRule} style={{ marginLeft: 5, display: rules.length === index + 1 ? '-webkit-inline-box' : 'none' }}>
-            Nova Regra
-          </Button>
-        </div>
-     ));
+      <div>
+        <AutoComplete
+          key={rule.name}
+          onSearch={this.handleSearch}
+          onChange={this.handleChange(rule)}
+          value={`${rule.name} ::= ${rule.value}`}
+        />
+        <Button size="small" title="Remover Regra" onClick={this.handleDeleteRule(rule)} icon="delete" type="danger" style={{ marginLeft: 10, display: rule.initial ? 'none' : '' }} />
+        <Button size="small" onClick={this.handleNewRule} style={{ marginLeft: 5, display: rules.length === index + 1 ? '-webkit-inline-box' : 'none' }}>
+          Nova Regra
+        </Button>
+      </div>
+    ));
   }
 
-  getNextRuleName = (current) => {
-    return String.fromCharCode(current.charCodeAt(0) + 1);
-  }
+  getNextRuleName = current => String.fromCharCode(current.charCodeAt(0) + 1)
 
   render() {
     return (
@@ -105,6 +108,6 @@ class GramaticaRegular extends Component {
 
 GramaticaRegular.propTypes = {
   onChangeRules: PropTypes.func.isRequired,
-}
+};
 
 export default GramaticaRegular;
